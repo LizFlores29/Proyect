65c272347f5b76bf3b41267356e622efd77b4a3f
LizFlores29 <lizanalyflores29@gmail.com>
2024-08-22 23:05:04 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 yesBtn.addEventListener('click',function (){
     alert('Cómo que no 😤😭, muy mal mi amor, ahora pon aceptar');
     alert('JAJAJA caíste, sé que no olvidaste este día, verdad?🥺, pon aceptar si no lo olvidaste');
-    alert('Ahora cuando pongas aceptar en este último mensaje, te llevará directamente a un drive, te diriges a proyecto pt2, no habras el documento "Carta" sólo descargalo y luego lo habres desde el mismo python, si lo habres desde el visual studio code, ya no funcionará la temática 🥺, así que has caso sip');
+    alert('Ahora cuando pongas aceptar en este último mensaje, te llevará directamente a un drive, te diriges a proyecto pt2, no abras el documento "Carta" sólo descargalo y luego lo abres desde el mismo python, si lo abres desde el visual studio code, ya no funcionará la temática 🥺, así que has caso sip');
     window.location.href = 'https://drive.google.com/drive/folders/1YblBaASV-rw5FnES2vrX436_2dNepwfZ';
 });
 
